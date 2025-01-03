8758c96c7342366a38c84757634b1367441b855a
finify <ifeanyiizuegbu@yahoo.com>
2025-01-04 00:19:59 +0100

    lesson 3

---
+++ stores/habits.js
@@ -0,0 +1,27 @@
+import { defineStore } from 'pinia'
+
+export const useHabitStore = defineStore('habitStore', {
+  state : () => ({
+    habits: [],
+  }),
+
+  actions: {
+    //fetching all habbits
+
+
+
+    //adding new habits 
+
+
+    //updating habits
+
+
+    //deleting habits
+
+
+    //completing 
+
+
+    //calculating streaks
+  }
+})
